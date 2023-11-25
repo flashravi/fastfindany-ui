f577ea7ae50f20898a817b7280711d74e3d15f8c
Ravi Madhavan <ravimadhavan@Ravis-MacBook-Pro.local>
2023-11-25 13:22:27 -0700

    https call

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const localurl = "http://localhost:8080/api";
-const ec2url = "http://18.218.53.124/api/";
+const ec2url = "https://18.218.53.124/api/";
 
 export default axios.create({
   baseURL: ec2url,
